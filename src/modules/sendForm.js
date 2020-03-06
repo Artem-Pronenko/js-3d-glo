e2a0e6c8ae41f63e127dfb66b287f4ab03f1724b
Artem Pronenko <pronenko.artem@gmail.com>
2020-03-06 16:46:10 +0200

    24-е доп

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -4,6 +4,18 @@ const sendForm = () => {
 		successMessage = 'Спасибо! Мы скоро с вами свяжемся!',
 		form = document.querySelectorAll('form'),
 		statusMessage = document.createElement('div');
+	let dataWidth = 0, dataInterval;
+
+	const dataAnimate = () => {
+		dataInterval = requestAnimationFrame(dataAnimate);
+		if (dataWidth < 350) {
+			dataWidth += 5;
+			statusMessage.style.width = dataWidth + 'px';
+		} else {
+			cancelAnimationFrame(dataInterval);
+			return false;
+		}
+	};
 
 	form.forEach(item => {
 		item.addEventListener('submit', (event) => {
@@ -13,9 +25,8 @@ const sendForm = () => {
 
 	const processingForm = (event, item) => {
 		event.preventDefault();
-		item.appendChild(statusMessage);
+		document.body.appendChild(statusMessage);
 		statusMessage.textContent = loadMessage;
-		statusMessage.style.color = '#fff';
 		const formData = new FormData(item);
 		let body = {};
 		for (let value of formData.entries()) body[value[0]] = value[1];
@@ -26,13 +37,20 @@ const sendForm = () => {
 				if (response.status !== 200) {
 					throw new Error('status network not 200');
 				}
+				statusMessage.classList.add('show-modal-data');
 				statusMessage.textContent = successMessage;
+				dataInterval = requestAnimationFrame(dataAnimate)
+
 			})
 			.catch(error => {
+				statusMessage.classList.add('show-modal-data');
 				statusMessage.textContent = errorMessage;
+				dataInterval = requestAnimationFrame(dataAnimate);
 				console.log(error);
 			})
+			.finally(() => setTimeout(() => statusMessage.remove(), 6000))
 	};
+
 
 	const postData = (body) => {
 		return fetch('./server.php', {
