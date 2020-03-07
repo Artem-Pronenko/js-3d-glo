d30737d9e24f5f7ae0b28c99bd57078d495afa66
Artem Pronenko <pronenko.artem@gmail.com>
2020-03-07 21:38:21 +0200

    правки с отправкой(скрытием) смс выполнил

--- src/modules/validForm.js
+++ src/modules/validForm.js
@@ -3,7 +3,7 @@ const validForm = (input) => {
 		item.addEventListener('input', () => {
 			const inputValue = item.value;
 			if (item.classList.contains('form-phone')) {
-				item.value = inputValue.replace(/[^0-9+]/i, '');
+				item.value = inputValue.replace(/^\+[^0-9]/, '');
 			} else if (item.classList.contains('form-name') || item.classList.contains('mess')) {
 				item.value = inputValue.replace(/[^а-яё ]/i, '');
 			}
